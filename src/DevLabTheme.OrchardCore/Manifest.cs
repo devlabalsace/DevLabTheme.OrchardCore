ba6f5daf1ac1959f73aaae14aa358065254f7ae9
Marina PHILIPPE <marina.philippe@outlook.com>
2019-09-06 10:41:41 +0200

    Possibilité de choisir l'image de fond à partir de l'admin du site pour les pages Article et Blog.

--- src/DevLabTheme.OrchardCore/Manifest.cs
+++ src/DevLabTheme.OrchardCore/Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "DevLab Theme",
     Author = "DevLab.Alsace Team",
     Website = "https://devlab.alsace",
-    Version = "1.0.4",
+    Version = "1.0.8",
     Description = "The theme for devlav.alsace site."
 )]
